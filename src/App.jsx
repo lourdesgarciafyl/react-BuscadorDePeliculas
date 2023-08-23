12ece17a085368606f26bb38961120808ac1c49e
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-23 18:40:58 -0300

    se maqueta el form con el input y el boton de buscar, tambien se hace responsive el div utilizado como contenedor de esos eleemntos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form} from 'react-bootstrap'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <section className='container'>
+      <h1 className='display-3 text-center'>Buscador de peliculas</h1>
+      <div className='text-center'>
+        <Form className='form mt-3 row justify-content-lg-center'>
+          <input className='col-md-6 me-3'
+          placeholder='StarWars, Sherk...'></input>
+          <Button className='col-md-2'>Buscar</Button>
+        </Form>
+      </div>
+    </section>
+    <section className='my-3 mainSection'>
+
+    </section>
+
     </>
   )
 }
